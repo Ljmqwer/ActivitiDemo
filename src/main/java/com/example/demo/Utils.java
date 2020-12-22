43f5e2a36f02a17903bc93482a349f88e0e58b1c
Ljmqwer <Lijiameng9818@163.com>
2020-12-22 14:47:35 +0800

    123

---
+++ src/main/java/com/example/demo/Utils.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+/**
+ * @Title: Utils
+ * @ProjectName ActivityDemo
+ * @Description: TODO
+ * @Author Li_jiaMeng
+ * @Date Created in 14:46 2020/12/22
+ */
+public class Utils {
+
+}
